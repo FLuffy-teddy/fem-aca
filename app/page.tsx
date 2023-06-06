c750677e9d00577c03b9c98b1218096b79150a24
Ty <ty@malkincreative.com>
2023-06-06 18:23:32 -0500

    no closer than when i started logic ways, but got the css looking better

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Inter } from "next/font/google";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,75 +9,49 @@ export default function Home() {
   function UserBirthday(e: { preventDefault: () => void }) {
     e.preventDefault();
 
-    const displayDay:number = subtractedDay;
-    const displayMonth:number = subtractedMonth;
-    const displayYear:number = subtractedYear;
+    const d = new Date();
 
-  setDisplayDay(displayDay);
+    d.setFullYear(year, month, day);
+
+    var difference = Math.abs(new Date().getTime() - d.getTime());
+    var time = new Date(difference);
+    console.log(time.toString());
   }
 
   const [day, setDay] = useState(0);
   const [month, setMonth] = useState(0);
   const [year, setYear] = useState(0);
-  const [displayday, setDisplayDay] = useState(0);
-  const [displaymonth, setDisplayMonth] = useState(0);
-  const [displayyear, setDisplayYear] = useState(0);
+  const [displayday, setDisplayDay] = useState("--");
+  const [displaymonth, setDisplayMonth] = useState("--");
+  const [displayyear, setDisplayYear] = useState("--");
 
-
-  let today = new Date();
-  const todaysYear = today.getFullYear();
-  const todaysMonth = today.getMonth() + 1;
-  const todaysDay = today.getDate();
-
-  let subtractedDay = todaysDay - day;
-  let subtractedMonth = todaysMonth - month;
-  let subtractedYear = todaysYear - year;
-  let maxDay;
-
-  //error check users numbers
-
-  //style page exactly as design
-
-  if (subtractedDay < 1) {
-    subtractedMonth = subtractedMonth - 1;
-
-    if ([1, 3, 5, 7, 8, 10, 12].includes(subtractedMonth)) {
-      subtractedDay = 31 + subtractedDay;
-      maxDay = 31;
-    }
-    if ([4, 6, 9, 11].includes(subtractedMonth)) {
-      subtractedDay = 30 + subtractedDay;
-      maxDay = 30;
-    }
-    if (subtractedMonth == 2) {
-      subtractedDay = 28 + subtractedDay;
-      maxDay = 28;
-    }
-  }
-  if (subtractedMonth < 1) {
-    subtractedYear = subtractedYear - 1;
-    subtractedMonth = 12;
-  }
+  const inputTitleColor = "text-offgrey";
+  const userInputColor = "text-offblack";
+  const calculatorColor = "text-purple";
+  const borderColor = "border-offwhite";
+  const buttonBG = "bg-purple";
 
   return (
     <main className="p-4 w-screen h-screen bg-slate-200 flex justify-center items-center">
-      <div className="border border-transparent rounded-xl flex flex-col bg-white text-slate-500">
-        <form className="flex border-b-2 p-8">
+      <div
+        className={`border border-transparent rounded-xl flex flex-col bg-white ${userInputColor}`}
+      >
+        <form className="flex border-b-2 p-8 relative">
           <div className="p-4 w-fit">
-            <h3 className="text-lg w-fit">Day</h3>
+            <h3 className={`${inputTitleColor} text-lg w-fit`}>Day</h3>
             <input
-              className="border border-slate-500 text-3xl w-1/2"
+              className={`border ${borderColor} text-3xl w-1/2`}
               name="day"
               placeholder="DD"
               type="number"
-              max={maxDay}
+              max={31}
               onChange={(e) => setDay(e.target.valueAsNumber)}
             ></input>
           </div>
           <div className="p-4 w-fit">
-            <h3 className="text-lg w-fit">Month</h3>
+            <h3 className={`${inputTitleColor} text-lg w-fit`}>Month</h3>
             <input
-              className="border border-slate-500 text-3xl w-1/2"
+              className={`border ${borderColor} text-3xl w-1/2`}
               name="month"
               placeholder="MM"
               type="number"
@@ -85,31 +60,39 @@ export default function Home() {
             ></input>
           </div>
           <div className="p-4 w-fit">
-            <h3 className="text-lg w-fit">Year</h3>
+            <h3 className={`${inputTitleColor} text-lg w-fit`}>Year</h3>
             <input
-              className="border border-slate-500 text-3xl w-1/2"
+              className={`border ${borderColor} text-3xl w-1/2`}
               name="year"
               placeholder="YYYY"
               type="number"
               max={2023}
               onChange={(e) => setYear(e.target.valueAsNumber)}
             ></input>
-            <button className="p-10 border border-black" onClick={UserBirthday}>
-              Input
-            </button>
           </div>
+          <button
+            className={`p-6 border-2 absolute rounded-full text-white right-0 top-3/4 ${buttonBG}`}
+            onClick={UserBirthday}
+          >
+            <Image
+              src="icon-arrow.svg"
+              alt="Button Arrow"
+              height={50}
+              width={50}
+            />
+          </button>
         </form>
         <div className="p-8">
           <div className="flex text-7xl">
-            <h4 className="text-violet-700">{}</h4>
+            <h4 className={`${calculatorColor}`}>{displayyear}</h4>
             <h2 className="text-black font-bold">years</h2>
           </div>
           <div className="flex text-7xl">
-            <h4 className="text-violet-700">{}</h4>
+            <h4 className={`${calculatorColor}`}>{displaymonth}</h4>
             <h2 className="text-black font-bold">months</h2>
           </div>
           <div className="flex text-7xl">
-            <h4 className="text-violet-700">{}</h4>
+            <h4 className={`${calculatorColor}`}>{displayday}</h4>
             <h2 className="text-black font-bold">days</h2>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        purple: "hsl(259, 100%, 65%)",
+        offwhite: "hsl(0, 0%, 94%)",
+        offblack: "hsl(0, 0%, 8%)",
+        offgrey: "hsl(0, 0%, 86%)",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
